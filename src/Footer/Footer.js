6bea2e281d4850119a84faf673e9c3ce44a32b8f
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-03-05 16:24:33 +0200

    add pagination

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -12,9 +12,29 @@ class Footer extends React.Component {
 
     render() {
         return (
-            <div className="footer">
-                footer
-      </div>
+            <div className="main-footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-xl-3 col-lg-3 col-md-6 col-sm-6 main-footer__block">
+                            <div className="title"><a href="/">Lorem, ipsum.</a> Posts</div>
+                            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores ad deleniti soluta. Dolore repellat delectus nihil iste, aperiam deleniti, vel cumque, repellendus esse nostrum necessitatibus enim? Voluptatem perspiciatis officia laborum nihil consectetur, eos dicta, culpa delectus laboriosam quia ex iste.</div>
+                        </div>
+                        <div className="col-12 col-xl-3 col-lg-3 col-md-6 col-sm-6 main-footer__block">
+                            <div className="title"><a href="/">Lorem, ipsum.</a> Posts</div>
+                            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores ad deleniti soluta. Dolore repellat delectus nihil iste, aperiam deleniti, vel cumque, repellendus esse nostrum necessitatibus enim? Voluptatem perspiciatis officia laborum nihil consectetur, eos dicta, culpa delectus laboriosam quia ex iste.</div>
+                        </div>
+                        <div className="col-12 col-xl-3 col-lg-3 col-md-6 col-sm-6 main-footer__block">
+                            <div className="title"><a href="/">Lorem, ipsum.</a> Posts</div>
+                            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores ad deleniti soluta. Dolore repellat delectus nihil iste, aperiam deleniti, vel cumque, repellendus esse nostrum necessitatibus enim? Voluptatem perspiciatis officia laborum nihil consectetur, eos dicta, culpa delectus laboriosam quia ex iste.</div>
+                        </div>
+                        <div className="col-12 col-xl-3 col-lg-3 col-md-6 col-sm-6 main-footer__block">
+                            <div className="title"><a href="/">Lorem, ipsum.</a> Posts</div>
+                            <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores ad deleniti soluta. Dolore repellat delectus nihil iste, aperiam deleniti, vel cumque, repellendus esse nostrum necessitatibus enim? Voluptatem perspiciatis officia laborum nihil consectetur, eos dicta, culpa delectus laboriosam quia ex iste.</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="main-footer__menu"></div>
+            </div>
         )
     }
 }
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -7,7 +7,9 @@ class Main extends React.Component {
         super(props)
 
         this.state = {
-            dataBase: ''
+            dataBase: '',
+            cardsPerPage: 12,
+            paginationList: '',
         }
         this.cards = '';
 
@@ -31,44 +33,75 @@ class Main extends React.Component {
         // for (let i = 0; i < data.length; i++) {
         for (let i = 0; i < baseFilledUpTo; i++) {
             let temp = {};
-            temp['data-id'] = data[i]['gsx$data-id']['$t'];
+            temp['id'] = data[i]['gsx$id']['$t'];
             temp['url'] = data[i]['gsx$url']['$t'];
             temp['author'] = data[i]['gsx$author']['$t'];
             temp['year'] = data[i]['gsx$year']['$t'];
             temp['name'] = data[i]['gsx$name']['$t'];
             temp['price'] = data[i]['gsx$price']['$t'];
             temp['rating'] = data[i]['gsx$rating']['$t'];
-            out[data[i]['gsx$data-id']['$t']] = temp;
+            out[data[i]['gsx$id']['$t']] = temp;
         }
         this.setState({ dataBase: out });
-        // console.log(this.state.dataBase)
+        this.pagination();
     }
 
+    cardItemClickEvent = (event) => {
+        console.log(event)
 
+    }
+
+    pagination = () => {
+        this.setState({ paginationList: Math.ceil(this.state.dataBase.length / this.state.cardsPerPage) });
+        // let list = 0;
+        return (
+            this.setState({
+                paginationList:
+                   <div className='container'>
+                        < ul className="pagination" >
+                            <li><a href="/">1</a></li>
+                            <li><a href="/">2</a></li>
+                            <li><a href="/">3</a></li>
+                        </ul >
+                   </div>
+            })
+        )
+    }
+
+    cardItem = (object) => {
+        return (
+            <div className="cards-item" key={object.id} data-id={object.id} onClick={this.cardItemClickEvent}>
+                <img className="cards-item__img" src={object.url} alt={object.name} />
+                <p className="cards-item__author" >{object.author}</p>
+                <p className="cards-item__year">{object.year}</p>
+                <p className="cards-item__name">{object.name}</p>
+                <p className="cards-item__price">{object.price}</p>
+                <p className="cards-item__rating">{object.rating}</p>
+            </div>
+        )
+    }
+
+    
     render() {
-
         if (this.state.dataBase[0]) {
             let out = []
             this.state.dataBase.forEach((object, i) => {
-                console.log(i)
-                return (
-                    out.push(<div className="cards-item" key={object.name + i} >
-                        <img className="cards-item__img" src={object.url} alt={object.name} />
-                        <div className="cards-item__author" >{object.author}</div>
-                        <div className="cards-item__year">{object.year}</div>
-                        <div className="cards-item__name">{object.name}</div>
-                        <div className="cards-item__price">{object.price}</div>
-                        <div className="cards-item__rating">{object.rating}</div>
-                    </div>)
-                )
+                if (i < this.state.cardsPerPage) {
+                    return (
+                        out.push(this.cardItem(object))
+                    )
+                }
             })
             this.cards = out;
         }
         return (
-            <div className="main">
+            <div className="main" >
                 main
-                <CardItem dataBase={this.state.dataBase} />
-                {this.cards}
+            < CardItem dataBase={this.state.dataBase} />
+                <div className="container card-item-container">
+                    {this.cards}
+                </div>
+                {this.state.paginationList}
             </div>
         )
     }
